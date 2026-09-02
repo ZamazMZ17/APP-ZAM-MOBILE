5f98c25873f820eae2a754947c302728f57d9878
ZamazMZ17 <u20221g355@upc.edu.pe>
2026-09-02 16:29:11 -0500

    chore: bump version to 0.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.zamazmz17.zammobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
     }
     signingConfigs {
         create("release") {
